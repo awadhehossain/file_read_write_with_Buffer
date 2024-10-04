005ffe7368a8109564faa325e6cab31abc8f05f5
Awadhe Hossain <149267112+awadhehossain@users.noreply.github.com>
2024-10-04 18:58:18 +0600

    Add files via upload

---
+++ CtoDWithoutNumber.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class CtoDWithoutNumber {
+    public static void main(String[] args) {
+        try{
+            FileReader file=new FileReader("src/c.text");
+            BufferedReader reader=new BufferedReader(file);
+            FileWriter file1=new FileWriter("src/d.text");
+            BufferedWriter writer=new BufferedWriter(file1);
+
+            int character;
+
+            // Read each character one by one
+            while ((character = reader.read()) != -1) {
+                if (!Character.isDigit(character)) {
+                    writer.write(character);
+                }
+            }
+            reader.close();
+            writer.close();
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+
+        }
+    }
+}
