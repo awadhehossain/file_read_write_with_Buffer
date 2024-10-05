7ab509137b5998fd31b47ea908edbf0abbb8da91
Awadhe Hossain <149267112+awadhehossain@users.noreply.github.com>
2024-10-05 10:07:27 +0600

    Update BufferWriter.java

--- BufferWriter.java
+++ BufferWriter.java
@@ -7,7 +7,7 @@ public class BufferWriter {
         try{
             FileWriter file=new FileWriter("src/output.txt");
             BufferedWriter writer=new BufferedWriter(file);
-            writer.write("My name is Awadhe Hossain.\n am a running student of Computer Science Department.\nMy university name is United  International University.\nIt is a popular university in Dhaka city.");
+            writer.write("My name is Awadhe Hossain.\nI am a running student of Computer Science Department.\nMy university name is United  International University.\nIt is a popular university in Dhaka city.");
 
            writer.close();
         }catch (IOException e){
